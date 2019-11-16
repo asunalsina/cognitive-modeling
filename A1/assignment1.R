4c311dde112ff2edc0577e86de4372f60b68f9af
Alsina <albayalsina34@gmail.com>
2019-11-16 11:34:34 +0100

    Update code

--- A1/assignment1.R
+++ A1/assignment1.R
@@ -1,9 +1,16 @@
 no_errors <- subset(keyPressDataWithLaneDeviation, typingErrorMadeOnTrial == 0)
+
+## Question 1
+# A
 total_time <- subset(no_errors, no_errors$phoneNrLengthAfterKeyPress == 11)
-
-
 steer_dial_mean <- aggregate(total_time$timeRelativeToTrialStart ,by=list(total_time$partOfExperiment),FUN=mean, na.rm=TRUE)
 steer_dial_sd <- aggregate(total_time$timeRelativeToTrialStart,by=list(total_time$partOfExperiment),FUN=sd, na.rm=TRUE)
 steer_dial_se <- steer_dial_sd[2] / sqrt(max(unique(total_time$pp)))
-
 #no_errors$timeRelativeToTrialStart[with(no_errors, no_errors$phoneNrLengthAfterKeyPress == 11)]
+
+# B
+lateral_deviation_mean <- aggregate(no_errors$lanePosition, by=list(no_errors$partOfExperiment),FUN=mean, na.rm=TRUE)
+lateral_deviation_sd <- aggregate(no_errors$lanePosition, by=list(no_errors$partOfExperiment),FUN=sd, na.rm=TRUE)
+lateral_deviation_se <- lateral_deviation_sd[2] / sqrt(max(unique(no_errors$pp)))
+
+# C

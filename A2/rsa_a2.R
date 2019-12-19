79fb1e66581783971fe9cc7b83bdaec6bdb6acca
Alsina <albayalsina34@gmail.com>
2019-12-20 00:01:02 +0100

    Update code

--- A2/rsa_a2.R
+++ A2/rsa_a2.R
@@ -62,28 +62,71 @@
     geom_tile() + scale_fill_viridis_c(option = "B") + xlab("") + ylab("")
 }
 
-## First column
+## First column t.test
 {
   first_column <- category_vectors[,1]
   first_column_matrix <- data.frame()
   for (i in first_column){
-    column_compared <- i == first_column
-    for(i in 1:length(column_compared)){
-      if(column_compared[i]){
-        column_compared[i] <- 1
-      }else{
-        column_compared[i] <- 0
-        }
-    }
-    first_column_matrix <- rbind(first_column_matrix, column_compared)
+    first_column_matrix <- rbind(first_column_matrix, i == first_column)
   }
-  matrix_without_diagonal <- first_column_matrix[lower.tri(first_column_matrix, diag = FALSE)]
-  matrix_neural_responses <- neural_responses[lower.tri(neural_responses, diag = FALSE)]
+  first_without_diagonal <- first_column_matrix[lower.tri(first_column_matrix, diag = FALSE)]
+  
+  # diagonal matrix
+  first_neural_responses <- neural_responses[lower.tri(neural_responses, diag = FALSE)]
   # animate = 1
-  neural_animate <- matrix_neural_responses[matrix_without_diagonal == 1]
+  first_neural_animate <- first_neural_responses[first_without_diagonal == 1]
   # inanimate = 0
-  neural_inanimate <- matrix_neural_responses[matrix_without_diagonal == 0]
-  animation_t_test <- t.test(neural_animate, neural_inanimate, paired = FALSE)
+  first_neural_inanimate <- first_neural_responses[first_without_diagonal == 0]
+  first_neural_responses_t_test <- t.test(first_neural_animate, first_neural_inanimate, paired = FALSE)
+  
+  # diagonal matrix
+  first_individual <- noise_responses[[1]][lower.tri(noise_responses[[1]], diag = FALSE)]
+  # animate = 1
+  first_individual_animate <- first_individual[first_without_diagonal == 1]
+  # inanimate = 0
+  first_individual_inanimate <- first_individual[first_without_diagonal == 0]
+  first_individual_t_test <- t.test(first_individual_animate, first_individual_inanimate, paired = FALSE)
+  
+  # diagonal matrix
+  first_average <- average_matrix[lower.tri(average_matrix, diag = FALSE)]
+  # animate = 1
+  first_average_animate <- first_average[first_without_diagonal == 1]
+  # inanimate = 0
+  first_average_inanimate <- first_average[first_without_diagonal == 0]
+  first_average_individual_t_test <- t.test(first_average_animate, first_average_inanimate, paired = FALSE)
 }
 
+## Sixth column t.test
+{
+  sixth_column <- category_vectors[,6]
+  sixth_column_matrix <- data.frame()
+  for (i in sixth_column){
+    sixth_column_matrix <- rbind(sixth_column_matrix, i == sixth_column)
+  }
+  sixth_without_diagonal <- sixth_column_matrix[lower.tri(sixth_column_matrix, diag = FALSE)]
+  
+  # diagonal matrix
+  sixth_neural_responses <- neural_responses[lower.tri(neural_responses, diag = FALSE)]
+  # animate = 1
+  sixth_neural_animate <- sixth_neural_responses[sixth_without_diagonal == 1]
+  # inanimate = 0
+  sixth_neural_inanimate <- sixth_neural_responses[sixth_without_diagonal == 0]
+  sixth_neural_responses_t_test <- t.test(sixth_neural_animate, sixth_neural_inanimate, paired = FALSE)
+  
+  # diagonal matrix
+  sixth_individual <- noise_responses[[1]][lower.tri(noise_responses[[1]], diag = FALSE)]
+  # animate = 1
+  sixth_individual_animate <- sixth_individual[sixth_without_diagonal == 1]
+  # inanimate = 0
+  sixth_individual_inanimate <- sixth_individual[sixth_without_diagonal == 0]
+  sixth_individual_t_test <- t.test(sixth_individual_animate, sixth_individual_inanimate, paired = FALSE)
+  
+  # diagonal matrix
+  sixth_average <- average_matrix[lower.tri(average_matrix, diag = FALSE)]
+  # animate = 1
+  sixth_average_animate <- sixth_average[sixth_without_diagonal == 1]
+  # inanimate = 0
+  sixth_average_inanimate <- sixth_average[sixth_without_diagonal == 0]
+  sixth_average_individual_t_test <- t.test(sixth_average_animate, sixth_average_inanimate, paired = FALSE)
+}
 
